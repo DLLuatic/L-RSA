8847fde09c64f9b0f642dfadf64315f642eb4c95
DLLuatic <fischerdavid196@gmail.com>
2024-01-22 00:05:41 +0100

    Fixed CRT algorithm, minor changes to test algortihm

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,19 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
 
         match input.trim() {
-            "whb" => {
-                println!("[main]: Testing WHB.");
-                let clock = Instant::now();
-                attacks::weak_hastad_broadcast::weak_hastad(vec![0, 3, 4], vec![3, 4, 5]);
-                let elapsed = clock.elapsed();
+            "whb100" => {
+                let mut all_t: u128 = 0;
+                for i in 0..99 {
+                    println!("[main]: Testing WHB.");
+                    let clock = Instant::now();
+                    let res = attacks::weak_hastad_broadcast::weak_hastad(vec![2, 3, 2], vec![3, 5, 7]);
+                    let elapsed = clock.elapsed();
 
-                println!("[main]: Time elapsed: {:?}", elapsed);
+                    all_t += clock.elapsed().as_micros();
+
+                    println!("[main]: Time elapsed: {:?}", elapsed);
+                }
+                println!("[main]: Average runtime: {}", all_t / 100);
             },
             "wiener" => {
                 println!("[main]: Testing Wiener.");
